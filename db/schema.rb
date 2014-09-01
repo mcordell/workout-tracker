e071991c1a3e2d96939f585e8eb1c388662a7c62
Michael Cordell <surpher@gmail.com>
2014-08-31 23:36:38 -0700

    Create exercises

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140830002631) do
+ActiveRecord::Schema.define(version: 20140901063612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "exercises", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
