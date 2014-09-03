a87b394fc6eea54be7ad613d575faff9442ee60c
Michael Cordell <surpher@gmail.com>
2014-09-02 17:30:53 -0700

    fixup! Basic view changes for creating a program

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,12 @@ Rails.application.routes.draw do
 
   resources :weights
 
-  resources :users do
+  devise_for :users do
     resources :programs
   end
 
   resources :exercises
 
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
