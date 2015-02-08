039d48c82aa62751357350e02707a24670d31d2b
Michael Cordell <surpher@gmail.com>
2015-02-08 10:55:07 -0800

    Finished testing program model methods

--- spec/models/program_spec.rb
+++ spec/models/program_spec.rb
@@ -45,4 +45,41 @@ describe Program do
       end
     end
   end
+
+  describe "#active" do
+    let!(:inactive_program) { FactoryGirl.create(:program, active: false) }
+    let!(:active_program) { FactoryGirl.create(:program, active: true) }
+    let!(:active_program2) { FactoryGirl.create(:program, active: true) }
+
+    it "returns active programs" do
+      expect(Program.active).to eq [active_program, active_program2]
+    end
+
+    it "does not return inactive programs" do
+      expect(Program.active).not_to include inactive_program
+    end
+  end
+
+  describe ".next_workout" do
+    context "when it does not have an unfinished cycle" do
+      it "returns nil" do
+        program_with_finished_cycles = FactoryGirl.create(:program, :with_finished_cycles)
+        expect(program_with_finished_cycles.next_workout).to be_nil
+      end
+    end
+
+    context "when it has an unfinished cycle" do
+      before do
+        subcycle = FactoryGirl.create(:subcycle)
+        complete_workout = FactoryGirl.create(:workout, :completed, subcycle: subcycle)
+        @incomplete_workout = FactoryGirl.create(:workout, :not_completed, subcycle: subcycle)
+        subcycle.cycle = FactoryGirl.create(:cycle, active: true, program: program)
+        subcycle.save
+      end
+
+      it "returns the next incomplete workout for that cycle" do
+        expect(program.next_workout).to eq @incomplete_workout
+      end
+    end
+  end
 end
