ad472cf87e27ecaf7ecbb186299c9b29c0fe972b
Michael Cordell <surpher@gmail.com>
2015-02-09 19:52:21 -0800

    Basic specs for cycle weight model

--- app/models/cycle_weight.rb
+++ app/models/cycle_weight.rb
@@ -11,14 +11,7 @@
 #
 
 class CycleWeight < ActiveRecord::Base
-
-  before_destroy :destroy_weight
-
   belongs_to :weight
   belongs_to :cycle
   accepts_nested_attributes_for :weight
-
-  def destroy_weight
-    self.weight.delete if self.weight
-  end
 end
---
+++ spec/models/cycle_weight_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+describe CycleWeight do
+
+end
