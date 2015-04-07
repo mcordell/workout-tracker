29a6e50418f83fa4a834ebaba366511d627c855f
Michael Cordell <surpher@gmail.com>
2015-04-06 22:59:36 -0700

    Remove accidentally committed test file

--- x.rb
+++
@@ -1,6 +0,0 @@
-
-cycle = Cycle.last
-program = FiveThreeOne::Program.new
-program.config.load_config_from_file(File.join(Rails.root, 'config', 'five_three_one.yml'))
-program.starting_weights = { deadlift: 320, press: 420, squat: 300, bench_press: 200 }
-cycle.copy_from_object(program.create_cycle)
