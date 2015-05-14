173fd606dfd52d64ea8acd43a10c328e027fc12a
Michael Cordell <surpher@gmail.com>
2015-05-13 22:43:48 -0700

    Add basic exercise list page tests

---
+++ spec/requests/managing_exercises_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe "managing exercises", type: :request do
+  let(:exercise_page) { ExercisePage.new }
+
+  context "when logged in" do
+    before do
+      login valid_user('test@email.com', 'password')
+    end
+
+    feature "visiting the list of exercises" do
+      let(:exercise_names) { %w{ Deadlift Squat} }
+
+      before do
+        @press_exercise = FactoryGirl.create(:exercise, name: 'Press')
+        exercise_names.each { |e| FactoryGirl.create(:exercise, name: e) }
+        exercise_page.goto
+      end
+
+      it "lists all of the exercise names with admin links" do
+        exercise_names.each do |name|
+          expect(exercise_page).to have_exercise_name(name)
+        end
+      end
+
+      feature "deleting a exercise" do
+        it "deletes the exercise" do
+          exercise_page.click_delete_exercise("Press")
+          expect(Exercise.find_by_name("Press")).to be_nil
+        end
+      end
+
+      feature "going to edit an exercise" do
+        it "goes to the edit page for the exercise" do
+          exercise_page.click_edit_exercise('Press')
+          expect(current_path).to eq edit_exercise_path(@press_exercise)
+        end
+      end
+
+      feature "going to show an exercise" do
+        it "goes to the show page for the exercise" do
+          exercise_page.click_show_exercise('Press')
+          expect(current_path).to eq exercise_path(@press_exercise)
+        end
+      end
+    end
+  end
+end
---
+++ spec/support/exercises_page.rb
@@ -0,0 +1,30 @@
+class ExercisePage < BasePage
+  SHOW_ICON = 'glyphicon-eye-open'
+  EDIT_ICON = 'glyphicon-pencil'
+  DELETE_ICON = 'glyphicon-trash'
+
+  def page_path
+    exercises_path
+  end
+
+  def has_exercise_name?(name)
+    has_css? 'td', text: name
+  end
+
+  def click_delete_exercise(exercise_name)
+    click_icon_in_row(exercise_name, DELETE_ICON)
+  end
+
+  def click_edit_exercise(exercise_name)
+    click_icon_in_row(exercise_name, EDIT_ICON)
+  end
+
+  def click_show_exercise(exercise_name)
+    click_icon_in_row(exercise_name, SHOW_ICON)
+  end
+
+  def click_icon_in_row(exercise_name, icon_class)
+    row = find(:xpath, "//tr[.//td[text()='#{exercise_name}']]")
+    row.find(:xpath, 'td/a[contains(concat(" ", normalize-space(@class), " "), " ' + icon_class + ' ")]').click
+  end
+end
