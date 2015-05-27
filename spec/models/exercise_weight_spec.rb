dd57a1c6164958ca04d648e949a5761260ee0b3d
Michael Cordell <surpher@gmail.com>
2015-05-27 13:29:22 -0700

    Modify shared weight specs to be more general
    Uses the subject, rather than instantiating an arbitrary weight.

--- spec/models/exercise_weight_spec.rb
+++ spec/models/exercise_weight_spec.rb
@@ -15,6 +15,9 @@ require 'rails_helper'
 
 describe ExerciseWeight do
 
+  let(:exercise_weight) { FactoryGirl.build(:exercise_weight, exercise_name: 'Something') }
+  subject { exercise_weight }
+
   it_behaves_like 'weight'
 
   it "delegates name to its exercise" do
@@ -23,7 +26,6 @@ describe ExerciseWeight do
   end
 
   context 'when the exercise is not present' do
-    let(:exercise_weight) { FactoryGirl.create(:exercise_weight, exercise_name: 'Something') }
     before { exercise_weight.exercise = nil }
 
     it "is not valid" do
--- spec/support/shared_examples_for_weights.rb
+++ spec/support/shared_examples_for_weights.rb
@@ -1,15 +1,14 @@
 RSpec.shared_examples "weight" do
-  let(:weight) { described_class.new(value: 70) }
+  before { subject.value = 70 }
 
   it "defaults to 'lbs' when a unit is not provided at creation" do
-    weight.unit = nil
-    weight.save
-    expect(weight.unit).to eq 'lbs'
+    subject.unit = nil
+    subject.save
+    expect(subject.unit).to eq 'lbs'
   end
 
-
   it "is not valid without a value" do
-    weight.value = nil
-    expect(weight).not_to be_valid
+    subject.value = nil
+    expect(subject).not_to be_valid
   end
 end
